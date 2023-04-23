5b09327ceac20f6a159e36de2b918d36874adb29
Andrew Bonafede <abonafede5@gmail.com>
2023-04-23 11:14:53 -0400

    adding structure

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 import streamlit as st
+
+def run_ui():
+    st.set_page_config(
+        page_title="Workout Helper",
+        page_icon="",
+        layout="wide")
+
+    st.title("Workout Helper")
+    st.markdown("""---""")
+    st.markdown("**Upload Files**")
+
+if __name__ == '__main__':
+    run_ui()
